3caf2d03398af73e982de61bfce5ca1ee330948a
Lucas Magalhães <imlucasmagalhaes@outlook.com>
2024-01-03 00:16:25 -0300

    fixing title and description of the modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,31 @@
 import Modal from './modal.js'
 
 const modal = Modal()
+let checkState = true
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
 
 //pegar todos os botões que existem com a classe check
 const checkButton = document.querySelectorAll(".actions a.check")
 
 checkButton.forEach(button => {
-    //adiciona a escuta
-    button.addEventListener("click", event => {
-        //abrir o modal
-        modal.open()
-    })
-})
+    button.addEventListener("click", (event) => {
+        checkState = !checkState;  // Inverte o valor de check a cada clique
+        handleClick(event, checkState);
+    });
+});
 
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-   button.addEventListener ("click", event => {
-        modal.open()
-   })
+   button.addEventListener("click", (event) => handleClick(event, false))
 })
+
+function handleClick(event, check){
+    modalTitle.innerHTML = check ? "Excluir essa pergunta" : "Marcar como lido"
+    modalDescription.innerHTML = check ? "Tem certeza que deseja excluir essa pergunta?" : "Tem certeza que deseja marcar como lida essa pergunta?"
+    //abrir modal
+    modal.open()
+}
